ab805f3f977fd3136a90654f6b0c32dc2ca4c24c
wjoansah <wonderful.owusu-ansah@amalitech.com>
2025-01-19 23:45:11 +0000

    update users function

--- src/handlers/users.mjs
+++ src/handlers/users.mjs
@@ -1,41 +1,56 @@
-import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
-import {DynamoDBDocumentClient, PutCommand, ScanCommand} from '@aws-sdk/lib-dynamodb';
-import {marshall} from "@aws-sdk/util-dynamodb";
-import uuid from "uuid";
+import crypto from 'node:crypto'
+import {
+    AdminCreateUserCommand,
+    CognitoIdentityProviderClient,
+    DeliveryMediumType,
+    ListUsersCommand
+} from '@aws-sdk/client-cognito-identity-provider'
+import {SFNClient, StartExecutionCommand} from "@aws-sdk/client-sfn";
 
-const client = new DynamoDBClient({});
-const ddbDocClient = DynamoDBDocumentClient.from(client);
+const cognitoClient = new CognitoIdentityProviderClient({})
+const sfnClient = new SFNClient({});
 
-const tableName = process.env.USERS_TABLE;
+const USER_POOL_ID = process.env.USER_POOL_ID
+const STATE_MACHINE_ARN = process.env.STATE_MACHINE_ARN
 
 export const handler = async (event, context) => {
     const routeKey = `${event['httpMethod']} ${event['resource']}`;
     let responseBody = {"message": "unsupported route"}
     let statusCode = 400;
 
-    let queryParams = {}
-
     try {
         if (routeKey === "GET /users") {
-            queryParams = {TableName: tableName};
-            responseBody["body"] = await ddbDocClient.send(new ScanCommand(queryParams));
-            statusCode = 200;
+            try {
+                const command = new ListUsersCommand({UserPoolId: USER_POOL_ID})
+                const response = await cognitoClient.send(command)
+                const users = response.Users
+
+                statusCode = response.statusCode
+                responseBody['body'] = users
+            } catch (err) {
+                statusCode = 500;
+                console.error(err)
+            }
         }
 
         if (routeKey === "POST /users/invite") {
             const body = JSON.parse(event.body);
             const {name, email} = body
-            const id = uuid.v7().toString();
-            const params = {
-                TableName: tableName,
-                Item: marshall({
-                    id,
-                    name,
-                    email
-                })
+
+            const createUserRequest = {
+                UserPoolId: USER_POOL_ID,
+                Username: email,
+                UserAttributes:
+                    [{Name: "name", Value: name},
+                        {Name: "email", Value: email},
+                        {Name: "email_verified", Value: "true"}],
+                TemporaryUserAttributes: generateTemporaryPassword(),
+                DesiredDeliveryMediums: [DeliveryMediumType.EMAIL]
             }
 
-            responseBody["body"] = await ddbDocClient.send(new PutCommand(queryParams));
+            await startUserOnboarding(email)
+
+            responseBody = await cognitoClient.send(new AdminCreateUserCommand(createUserRequest))
             statusCode = 201;
         }
 
@@ -48,5 +63,24 @@ export const handler = async (event, context) => {
     return {
         statusCode,
         body: responseBody,
+    }
+}
+
+const generateTemporaryPassword = () => {
+    return crypto.randomBytes(16).toString("hex")
+}
+
+const startUserOnboarding = async (userEmail) => {
+    const input = `{"workflowType":"onboarding","userEmail":"${userEmail}"}"}`
+    const params = {
+        stateMachineArn: STATE_MACHINE_ARN,
+        input
+    }
+    const command = new StartExecutionCommand(params)
+
+    try {
+        await sfnClient.send(command)
+    } catch (error) {
+        console.error(error)
     }
 }
