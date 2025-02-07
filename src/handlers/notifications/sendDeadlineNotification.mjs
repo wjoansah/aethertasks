b90c3ca43853801e26224238f9c1ce6891413298
wjoansah <wonderful.owusu-ansah@amalitech.com>
2025-02-07 19:13:58 +0000

    update message template

--- src/handlers/notifications/sendDeadlineNotification.mjs
+++ src/handlers/notifications/sendDeadlineNotification.mjs
@@ -41,7 +41,7 @@ export const handler = async (event) => {
                 new PublishCommand({
                     TopicArn: taskDeadlineTopicArn,
                     Subject: `Task Deadline - ${item.name}`,
-                    Message: `Task "${item.name}" is due in 1 hour.`,
+                    Message: `Task "${item.name}" is due in 1 hour.\n\nPlease take the necessary actions to address this.\n\nBest Regards,\nAetherTasks Team`,
                     MessageAttributes: {
                         responsibility: {
                             DataType: "String",
